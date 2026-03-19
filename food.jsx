e0c69b22650ce06655dc77da0e96c61dfdd1b8e7
RVboutabagg <rolandovega2007@gmail.com>
2026-03-19 14:27:08 -0700

    add2

---
+++ food.jsx
@@ -0,0 +1,14 @@
+function Food() {
+    const food1 = "Pizza";
+    const food2 = "Sushi";
+    const food3 = "Tacos";
+    
+    return(
+      <div>
+        
+          </div>
+    ) 
+    }
+    
+    export default Food;
+    
